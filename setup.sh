d574942451b45078b351a1f9b4ef7b97382cccef
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-15 18:39:17 -0500

    Update `setup.sh` to set up PKG_CONFIG_PATH

--- setup.sh
+++ setup.sh
@@ -1,39 +1,44 @@
 unset PATH
 for p in $baseInputs $buildInputs; do
-	export PATH=$p/bin${PATH:+:}$PATH
+    if [ -d $p/bin ]; then
+        export PATH="$p/bin${PATH:+:}$PATH"
+    fi
+    if [ -d $p/lib/pkgconfig ]; then
+        export PKG_CONFIG_PATH="$p/lib/pkgconfig${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
+    fi
 done
 
 unpackPhase() {
-	tar xzf $src
+    tar xzf $src
 
-	for d in *; do
-		if [ -d "$d" ]; then
-			cd "$d"
-			break
-		fi
-	done
+    for d in *; do
+        if [ -d "$d" ]; then
+            cd "$d"
+            break
+        fi
+    done
 }
 
 configurePhase() {
-	./configure --prefix=$out
+    ./configure --prefix=$out
 }
 
 buildPhase() {
-	make
+    make
 }
 
 installPhase() {
-	make install
+    make install
 }
 
 fixupPhase() {
-	find $out -type f -exec patchelf --shrink-rpath '{}' \; -exec strip '{}' \; 2>/dev/null
+    find $out -type f -exec patchelf --shrink-rpath '{}' \; -exec strip '{}' \; 2>/dev/null
 }
 
 genericBuild() {
-	unpackPhase
-	configurePhase
-	buildPhase
-	installPhase
-	fixupPhase
+    unpackPhase
+    configurePhase
+    buildPhase
+    installPhase
+    fixupPhase
 }
