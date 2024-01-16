7979af660c0c78c5b04dc2ae0817944bac00c46b
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-16 00:39:43 -0500

    Update `hello.nix` to be a function

--- hello.nix
+++ hello.nix
@@ -1,8 +1,5 @@
-let
-  pkgs = import <nixpkgs> {};
-  mkDerivation = import ./autotools.nix pkgs;
-in
-  mkDerivation {
-    name = "hello";
-    src = ./hello-2.12.1.tar.gz;
-  }
+mkDerivation:
+mkDerivation {
+  name = "hello";
+  src = ./hello-2.12.1.tar.gz;
+}
