e8f7c14ecad96fa17bdbb3e88bf6b1e16e37c91c
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-16 00:40:00 -0500

    Update `default.nix` to be inputs focused

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
-{
-  graphviz = import ./graphviz.nix;
-  hello = import ./hello.nix;
-}
+let
+  pkgs = import <nixpkgs> {};
+  mkDerivation = import ./autotools.nix pkgs;
+in
+  with pkgs; {
+    hello = import ./hello.nix mkDerivation;
+
+    graphviz = import ./graphviz.nix {inherit mkDerivation lib pkg-config expat gd;};
+
+    graphvizCore = import ./graphviz.nix {
+      inherit mkDerivation lib pkg-config expat gd;
+      gdSupport = false;
+    };
+  }
